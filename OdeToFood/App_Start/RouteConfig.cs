22e25f701380749ff49d4d980adbbd2d48150e3c
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-03-07 18:47:40 -0300

    Actions and Parameters

--- OdeToFood/App_Start/RouteConfig.cs
+++ OdeToFood/App_Start/RouteConfig.cs
@@ -12,7 +12,18 @@ namespace OdeToFood
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.LowercaseUrls = true;
 
+            // /Cuisine
+            routes.MapRoute("Cuisine",
+                "cuisine/{name}",
+                /*
+                 * With 'UrlParameter.Optional' if name is not defined, the route
+                 * won't crash and I can also declare an optional name at the controller
+                 */
+                new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional });
+
+            // /Home
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
