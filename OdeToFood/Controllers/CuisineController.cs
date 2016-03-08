f33f6ba91e7baea69621eaa4041874109123d643
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-03-08 18:00:17 -0300

    Action Selectors

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -9,15 +9,30 @@ namespace OdeToFood.Controllers
     public class CuisineController : Controller
     {
         // GET: Cuisine
+        [HttpGet]
         public ActionResult Search(string name = "french")
         {
             var message = Server.HtmlEncode(name);
 
             // I could use a FileResult to return PDF files, spreadsheets, etc.
+            // i.e. return File(Server.MapPath("~/Content/site.css"), "text/css");
+
+            // Or a permanent redirect (http 301)
+            // i.e. return RedirectPermanent("http://www.google.com");
+
+            // Or an action with a parameter as defined at RouteConfig
+            // return RedirectToAction("Index", "Home", new { name = name });
+
+            // Or a file
             // return File(Server.MapPath("~/Content/site.css"), "text/css");
 
             // Or a json file
-            return Json(new { Message = message, Name = "Scott" }, JsonRequestBehavior.AllowGet);
+            // return Json(new { Message = message, Name = "Scott" }, JsonRequestBehavior.AllowGet);
+
+            // Note: a method with the same "signature" is a method with same name and
+            // parameters
+
+            return Content(message);
         }
     }
 }
